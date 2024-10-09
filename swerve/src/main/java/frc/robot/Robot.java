f13a0679ece2f465d7226760d9e71653726fb2ae
cmdada <cmdada@proton.me>
2024-10-09 10:46:01 -0600

    makes the bot more responsive

--- swerve/src/main/java/frc/robot/Robot.java
+++ swerve/src/main/java/frc/robot/Robot.java
@@ -14,9 +14,9 @@ public class Robot extends LoggedRobot {
   // Call the intake.java file that just runs one talon srx for now but if we have a multi motor shooter i'll add more
   private final Intake m_intake = new Intake(10, m_controller); // shooter motorcontroller will be can id 9
   
-  // Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0 to 1.
-  private final SlewRateLimiter m_xspeedLimiter = new SlewRateLimiter(3);
-  private final SlewRateLimiter m_yspeedLimiter = new SlewRateLimiter(3);
+  // Slew rate limiters to make joystick inputs more gentle; 1/4 sec from 0 to 1 for drive and 1/3 sec for turning.
+  private final SlewRateLimiter m_xspeedLimiter = new SlewRateLimiter(4);
+  private final SlewRateLimiter m_yspeedLimiter = new SlewRateLimiter(4);
   private final SlewRateLimiter m_rotLimiter = new SlewRateLimiter(3);
   private final Drivetrain m_drivetrain = new Drivetrain();
 
